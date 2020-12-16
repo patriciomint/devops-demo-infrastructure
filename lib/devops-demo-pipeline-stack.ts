358d26f007b871b973d291c2c760f180a262d3af
Patricio Marrone <marrone.patricio.test@gmail.com>
2020-12-16 22:50:57 +0100

    Creating an ECR repo for project images

--- lib/devops-demo-pipeline-stack.ts
+++ lib/devops-demo-pipeline-stack.ts
@@ -1,9 +1,23 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as codebuild from '@aws-cdk/aws-codebuild'
+import * as codecommit from '@aws-cdk/aws-codecommit'
+import * as codepipeline from '@aws-cdk/aws-codepipeline'
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
+import * as lambda from '@aws-cdk/aws-lambda'
+import  * as ecr from '@aws-cdk/aws-ecr'
+import { App, Stack, StackProps } from '@aws-cdk/core'
+
+export interface PipelineStackProps extends StackProps {
+  readonly lambdaCode: lambda.CfnParametersCode;
+  readonly repoName: string
+}
 
 export class DevopsDemoPipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props?: PipelineStackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const repository = new ecr.Repository(this, 'DevopsDemo', {
+      repositoryName: 'devops-demo',
+    });
   }
 }
